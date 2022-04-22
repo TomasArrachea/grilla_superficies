21bdd75b31374a248dbc256d886305530988ae34
tomasarrachea <tarrachea@fi.uba.ar>
2022-04-22 03:09:36 -0300

    Grilla y superficies de plano, esfera y tubo senoidal

---
+++ modulo-geometria.js
@@ -0,0 +1,231 @@
+
+
+/*
+
+    Tareas:
+    ------
+
+    1) Modificar a función "generarSuperficie" para que tenga en cuenta los parametros filas y columnas al llenar el indexBuffer
+       Con esta modificación deberían poder generarse planos de N filas por M columnas
+
+    2) Modificar la funcion "dibujarMalla" para que use la primitiva "triangle_strip"
+
+    3) Crear nuevos tipos funciones constructoras de superficies
+
+        3a) Crear la función constructora "Esfera" que reciba como parámetro el radio
+
+        3b) Crear la función constructora "TuboSenoidal" que reciba como parámetro la amplitud de onda, longitud de onda, radio del tubo y altura.
+        (Ver imagenes JPG adjuntas)
+        
+        
+    Entrega:
+    -------
+
+    - Agregar una variable global que permita elegir facilmente que tipo de primitiva se desea visualizar [plano,esfera,tubosenoidal]
+    
+*/
+var superficie3D;
+var mallaDeTriangulos;
+
+
+function crearGeometria(){
+        
+    if (tipoSuperficie == 'plano') {
+        superficie3D=new Plano(filas,columnas);
+    } else if (tipoSuperficie == 'esfera') {
+        superficie3D=new Esfera(1.5)
+    } else if (tipoSuperficie == 'tubosenoidal') {
+        superficie3D=new TuboSenoidal(0.1, 0.5, 1, 5)
+    }
+    mallaDeTriangulos=generarSuperficie(superficie3D,filas,columnas);
+    
+}
+
+function dibujarGeometria(){
+
+    dibujarMalla(mallaDeTriangulos);
+
+}
+
+function Plano(ancho,largo){
+
+    this.getPosicion=function(u,v){
+
+        var x =(u-0.5)*ancho;
+        var z=(v-0.5)*largo;
+        return [x,0,z];
+    }
+
+    this.getNormal=function(u,v){
+        return [0,1,0];
+    }
+
+    this.getCoordenadasTextura=function(u,v){
+        return [u,v];
+    }
+}
+
+function Esfera(radio){
+    
+    this.getPosicion=function(u,v){
+        // Pasaje coordenadas esfericas a cartesianas. u y v son los angulos.
+        // u y v son numeros de 0 a 1 -> los paso a radianes
+        u = u * 2*Math.PI
+        v = v * 2*Math.PI
+        var x = radio * Math.sin(u) * Math.cos(v);
+        var y = radio * Math.sin(u) * Math.sin(v);
+        var z = radio * Math.cos(u);
+        return [x,y,z];
+    }
+
+    this.getNormal=function(u,v){
+        // La normal es el vector posicion normalizado (dividir cada elemento del vector por su norma)
+        var pos = this.getPosicion(u,v);
+        var len = Math.sqrt(pos[0]**2 + pos[1]**2 + pos[2]**2);
+        return [pos[0]/len, pos[1]/len, pos[2]/len];
+    }
+
+    this.getCoordenadasTextura=function(u,v){
+        return [u,v];
+    }
+}
+
+function TuboSenoidal(amplitud, longitud, radio, altura){
+    
+    this.getPosicion=function(u,v){
+        // Pasaje de coordenadas cilindricas a cartesianas agregando el termino del tubo senoidal.
+        // u es el angulo y v es la fraccion de la altura (medida con el centro del tubo en 0)
+        // u es un numero de 0 a 1 -> lo paso a radianes
+        u = u * 2*Math.PI
+        var z = altura/2 * (v - 1/2);
+        var x = radio * Math.sin(u) + amplitud * Math.cos(z*2*Math.PI/longitud) * Math.sin(u);
+        var y = radio * Math.cos(u) + amplitud * Math.cos(z*2*Math.PI/longitud) * Math.cos(u);
+        return [x,y,z];
+    }
+
+    this.getNormal=function(u,v){
+        // En x e y la normal es igual al vector de posicion.
+        var pos = this.getPosicion(u,v);
+
+        // en z, la normal apunta en la direccion -1/m, siendo m la derivada del coseno -> seno.
+        v = altura/2 * (v - 1/2)
+        pos[2] = -1 / (amplitud * Math.sin(v*2*Math.PI/longitud))
+
+        return [pos[0],pos[1],pos[2]];
+    }
+
+    this.getCoordenadasTextura=function(u,v){
+        return [u,v];
+    }
+}
+
+
+function generarSuperficie(superficie,filas,columnas){
+    
+    positionBuffer = [];
+    normalBuffer = [];
+    uvBuffer = [];
+
+    for (var i=0; i < filas; i++) {
+        for (var j=0; j < columnas; j++) {
+
+            var u=j/columnas;
+            var v=i/filas;
+
+            var pos=superficie.getPosicion(u,v);
+
+            positionBuffer.push(pos[0]);
+            positionBuffer.push(pos[1]);
+            positionBuffer.push(pos[2]);
+
+            var nrm=superficie.getNormal(u,v);
+
+            normalBuffer.push(nrm[0]);
+            normalBuffer.push(nrm[1]);
+            normalBuffer.push(nrm[2]);
+
+            var uvs=superficie.getCoordenadasTextura(u,v);
+
+            uvBuffer.push(uvs[0]);
+            uvBuffer.push(uvs[1]);
+
+        }
+    }
+
+    // Buffer de indices de los triángulos
+    indexBuffer=[];  
+
+    for (i=0; i < filas - 1; i++) {
+        for (j=0; j < columnas; j++) {
+            indexBuffer.push(j + columnas*i);
+            indexBuffer.push(j + columnas*(i+1));
+        }
+        // Al final de la columna degenera los triangulos (menos en la ultima fila)
+        if (i < filas - 2) {
+            indexBuffer.push(columnas-1 + columnas*(i+1));
+            indexBuffer.push(columnas + columnas*i);
+        }   
+    }
+    
+    // Creación e Inicialización de los buffers
+
+    webgl_position_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, webgl_position_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positionBuffer), gl.STATIC_DRAW);
+    webgl_position_buffer.itemSize = 3;
+    webgl_position_buffer.numItems = positionBuffer.length / 3;
+
+    webgl_normal_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, webgl_normal_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normalBuffer), gl.STATIC_DRAW);
+    webgl_normal_buffer.itemSize = 3;
+    webgl_normal_buffer.numItems = normalBuffer.length / 3;
+
+    webgl_uvs_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, webgl_uvs_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvBuffer), gl.STATIC_DRAW);
+    webgl_uvs_buffer.itemSize = 2;
+    webgl_uvs_buffer.numItems = uvBuffer.length / 2;
+
+
+    webgl_index_buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, webgl_index_buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indexBuffer), gl.STATIC_DRAW);
+    webgl_index_buffer.itemSize = 1;
+    webgl_index_buffer.numItems = indexBuffer.length;
+
+    return {
+        webgl_position_buffer,
+        webgl_normal_buffer,
+        webgl_uvs_buffer,
+        webgl_index_buffer
+    }
+}
+
+function dibujarMalla(mallaDeTriangulos){
+    
+    // Se configuran los buffers que alimentaron el pipeline
+    gl.bindBuffer(gl.ARRAY_BUFFER, mallaDeTriangulos.webgl_position_buffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, mallaDeTriangulos.webgl_position_buffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, mallaDeTriangulos.webgl_uvs_buffer);
+    gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, mallaDeTriangulos.webgl_uvs_buffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, mallaDeTriangulos.webgl_normal_buffer);
+    gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, mallaDeTriangulos.webgl_normal_buffer.itemSize, gl.FLOAT, false, 0, 0);
+       
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, mallaDeTriangulos.webgl_index_buffer);
+
+
+    if (modo!="wireframe"){
+        gl.uniform1i(shaderProgram.useLightingUniform,(lighting=="true"));                    
+        gl.drawElements(gl.TRIANGLE_STRIP, mallaDeTriangulos.webgl_index_buffer.numItems, gl.UNSIGNED_SHORT, 0);
+    }
+    
+    if (modo!="smooth") {
+        gl.uniform1i(shaderProgram.useLightingUniform,false);
+        gl.drawElements(gl.LINE_STRIP, mallaDeTriangulos.webgl_index_buffer.numItems, gl.UNSIGNED_SHORT, 0);
+    }
+ 
+}
+
